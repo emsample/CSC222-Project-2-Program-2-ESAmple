3f265242d593557884f68624634249d9a303a1e0
emsample <esamplebox@gmail.com>
2026-03-08 12:12:06 -0400

    Setting up to get started properly, I added my global constants we'll see if the students one sticks around.

---
+++ CSC222 Project 2 Program 2 ESAmple/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+using namespace std;
+const int tests = 5;
+const int students = 150;
+int main()
+{
+	ifstream infile("StudentGrades.txt");
+
+}
