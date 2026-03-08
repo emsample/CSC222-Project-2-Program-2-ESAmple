7c70349cc29dcbfb8631b346366adb2725d2d001
emsample <esamplebox@gmail.com>
2026-03-08 13:07:29 -0400

    Added my average finder and letter grade assigning function. Average finder uses nested for loops to run through each grade for each student, sums them and divides by the global tests variable.  The other was also simple, I just ran a loop that compares each average in the GPA array to the if statement that checks for a value range. That made filling out the array of letter grades super simple. Made the average finding function. I got to do a ranged based loop since tests is a constant which is cool. I shouldn't need anything as a return so void should be fine. I also casted the sum as a double just to avoid truncation of the averages, I'm giving an 89.5 an 'A'

--- CSC222 Project 2 Program 2 ESAmple/main.cpp
+++ CSC222 Project 2 Program 2 ESAmple/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 const int tests = 5;
 const int students = 150;
 bool readData(string[], int[][tests], int&);
+void avgFinder(double[], const int[][tests], int&);
+void gradeAssigned(char[], double[], int&);
 int main()
 {
 	int number = 0;
@@ -23,4 +25,30 @@ bool readData(string llamo, int grades[][tests], int& total)
 	while (total < students && infile >> llamo[total] >> grades[total][0] >> grades[total][1] >> grades[total][2]) //thought about a for loop but this was less typing and theoretically works
 		total++;
 	return true;
+}
+void avgFinder(double gpa[], const int data[][tests], int& size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int sum = 0;
+		for (int data : data[i])
+			sum += data;
+		gpa[i] = static_cast<double>(sum) / tests;        //wanted to avoid integer division, an 89.5 is an A in my gradebook
+	}
+}
+void gradeAssigned(char book[], double final[], int& max)
+{
+	for (int i = 0 ; i<max ; i++)
+	{
+		if (final[i] > 89.5)
+			book[i] = 'A';
+		if (final[i] > 79.5 && final[i] < 89.5)
+			book[i] = 'B';
+		if (final[i] > 69.5 && final[i] < 79.5)
+			book[i] = 'C';
+		if (final[i] > 59.5 && final[i] < 69.5)
+			book[i] = 'D';
+		if (final[i] < 59.5)
+			book[i] = 'F';
+	}
 }
