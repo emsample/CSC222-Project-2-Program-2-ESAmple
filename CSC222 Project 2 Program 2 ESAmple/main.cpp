f45c612888857d8d3ffd0e521f0f8e44fca02e88
emsample <esamplebox@gmail.com>
2026-03-08 12:49:03 -0400

    Made my file reading function, it should be storing the data in teh appropriate array's. We'll see when I actually start displaying stuff.

--- CSC222 Project 2 Program 2 ESAmple/main.cpp
+++ CSC222 Project 2 Program 2 ESAmple/main.cpp
@@ -3,13 +3,24 @@
 #include <fstream>
 #include <string>
 using namespace std;
-bool readData();
 const int tests = 5;
 const int students = 150;
+bool readData(string[], int[][tests], int&);
 int main()
 {
+	int number = 0;
 	string names[students];
 	int scores[students][tests];
 	double average[students];
 	char lettergrade[students];
+}
+bool readData(string llamo, int grades[][tests], int& total)
+{
+	total = 0;
+	ifstream infile("StudenGrades.txt");
+	if (!infile)
+		return false;
+	while (total < students && infile >> llamo[total] >> grades[total][0] >> grades[total][1] >> grades[total][2]) //thought about a for loop but this was less typing and theoretically works
+		total++;
+	return true;
 }
